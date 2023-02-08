207d09a13f08f44c3182474cf6840aef0298f9b6
Ayah Elsais <elsais.ayah@gmail.com>
2023-02-07 22:49:56 -0500

    3 deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+const [items, setItems] = useState([]);
+
+useEffect(() => {
+fetch("http://localhost:6001/listings")
+.then(response => response.json())
+.then(data => setItems(data))
+},[])
+// console.log(items);
+
+// const [removedItems, setRemovedItems] = useState([]);
+function handleDelete(id){
+const filteredArray = items.filter((item) => item.id !== id)
+setItems(filteredArray)
+}
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer items = {items} handleDelete = {handleDelete}/>
     </div>
   );
 }
